bb951683a3d1ddb774e683684a529b39240c9c89
Jackson Johanek <jack.johanek@gmail.com>
2024-07-27 22:11:27 -0500

    Two initial testing spirit stat blocks

---
+++ spirit_data.js
@@ -0,0 +1,29 @@
+export const LUJI = {
+    stats:{
+        presence:5,
+        health:4,
+        speed:2,
+        luck:3,
+        knowledge:4,
+        initiative:6,
+        inventory:1,
+        strike:1
+    },
+    passiveAbility:"He does not need to reveal himself when he fails to Scout or Venture a Rift Room. When another spirit reveals themself in a Poles of Will room, Luji gets one +1 to Health.",
+    activeAbility:"May spotlight both of the Poles of Will rooms.",
+    }
+
+export const SHUJA = {
+    stats:{
+        presence:3,
+        health:2,
+        speed:2,
+        luck:6,
+        knowledge:4,
+        initiative:5,
+        inventory:1,
+        strike:1
+    },
+    passiveAbility:"When Shuja successfully loots, he may choose to reroll which item he gets within the tier once per successful loot.",
+    activeAbility:"May double all his luck die before rolling on his next turn.",
+}
